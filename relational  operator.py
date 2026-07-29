8ad06a624dfb160098711ca5c5e3615dd5c043d6
Naveen Kayakokula <naveenkayakokula@gmail.com>
2026-07-29 11:21:36 +0530

    practice problems

--- relational  operator.py
+++ relational  operator.py
@@ -1,11 +1,34 @@
 class Bank:
-    def __init__(self,name,acc):
-        self.name=name
-        self.acc=acc
-        self.bal=0
-    def __hash__(self):
-        return hash(self.acc)
-b1=Bank("Naveen",2000)
-b2=Bank("Ram",3000)
-k={b1,b2}
-print(b1==b2)
+    def __init__(self, accno, name, pin, balance=0):
+        self.accno = accno
+        self.name = name
+        self.pin = pin
+        self.balance = balance
+    def __add__(self, amount):
+        self.balance += amount
+        print(f"{self.balance} is deposited to bank")
+    def validate_pin(self, entered_pin):
+        return self.pin == entered_pin
+    def __sub__(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"{amount} is withdraw from bank")
+            print(f"Total balance:{self.balance}")
+        else:
+            print("Insufficient balance!")
+    def __call__(self):
+        print(f"Account No: {self.accno}, Name: {self.name}, Balance: {self.balance}")
+    def __gt__(self, other):
+        return self.balance > other.balance
+    def __repr__(self):
+        return f"Account No: {self.accno}, Name: {self.name}, Balance: {self.balance}"
+b1 = Bank(992200101, "Naveen", 1234)
+b2 = Bank(992200102, "Karthik", 5678)
+
+b1 + 50000
+if b1.validate_pin(1234):
+    b1 - 35000
+b1()
+l=[b1,b2]
+print(l)
+print(b1 > b2)
